958dc059570b9956bfdae1cba2029135a8d6209f
Matt Lavelle <105445756+Mattlavelle966@users.noreply.github.com>
2022-11-29 13:17:08 -0700

    scroll pos complete
    scroll position will now be saved in clients local storage and will stay the same regardless of what page you are on or go on.

--- window-scroll-const.js
+++ window-scroll-const.js
@@ -1,4 +1,12 @@
 window.addEventListener("scroll", (event) => {
+    event.preventDefault();
     let scroll = this.scrollY;
     console.log(scroll)
+    localStorage.setItem("scroll-pos", scroll)
+    return scroll;
+});
+window.addEventListener("load", () => {
+    console.log("ahead")
+    const pos = localStorage.getItem('scroll-pos');
+    scrollTo(0, pos);
 });
