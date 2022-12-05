34a2c9a55ca8822074ccb304e70babb2ac11d495
Matt Lavelle <105445756+Mattlavelle966@users.noreply.github.com>
2022-12-05 12:07:04 -0700

    First-open.js and linking in other files
    First-open.js is for keeping track of the first time they are opening a website it is a key value pair in local storage.

--- window-scroll-const.js
+++ window-scroll-const.js
@@ -2,13 +2,11 @@
 window.addEventListener("scroll", (event) => {
     event.preventDefault();
     let scroll = this.scrollY;
-    console.log(scroll)
     localStorage.setItem("scroll-pos", scroll)
     return scroll;
 });
 //when the window begins to load it will get scroll position from local storage and then move to that posotion. 
 window.addEventListener("load", () => {
-    console.log("ahead")
     const pos = localStorage.getItem('scroll-pos');
     scrollTo(0, pos);
 });
