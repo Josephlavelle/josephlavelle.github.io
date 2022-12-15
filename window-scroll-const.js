3c26d1f4c2ecf5b8e3a3b8a22ad4c3ebe12a093e
Matt Lavelle <105445756+Mattlavelle966@users.noreply.github.com>
2022-12-15 15:15:38 -0700

    trying to speed up scroll-const.js

--- window-scroll-const.js
+++ window-scroll-const.js
@@ -1,3 +1,4 @@
+const pos = localStorage.getItem('scroll-pos');
 //gets the scroll position when adjusted and saves scroll position to local storge.
 window.addEventListener("scroll", (event) => {
     event.preventDefault();
@@ -7,6 +8,5 @@ window.addEventListener("scroll", (event) => {
 });
 //when the window begins to load it will get scroll position from local storage and then move to that posotion. 
 window.addEventListener("load", () => {
-    const pos = localStorage.getItem('scroll-pos');
     scrollTo(0, pos);
 });
